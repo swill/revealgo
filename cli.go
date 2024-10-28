5d47f28a26b2678b3978d77791dbe110303f962e
Will Stevens <wstevens@cloudops.com>
2024-10-28 18:40:52 -0400

    added an aptum theme

--- cli.go
+++ cli.go
@@ -15,7 +15,7 @@ type CLI struct {
 
 type CLIOptions struct {
 	Port         int    `short:"p" long:"port" description:"tcp port number of this server. default is 3000."`
-	Theme        string `long:"theme" description:"slide theme or original css file name. default themes: beige, black, blood, league, moon, night, serif, simple, sky, solarized, and white" default:"cloudops.css"`
+	Theme        string `long:"theme" description:"slide theme or original css file name. default themes: beige, black, blood, league, moon, night, serif, simple, sky, solarized, and white" default:"aptum.css"`
 	Transition   string `long:"transition" description:"transition effect for slides: default, cube, page, concave, zoom, linear, fade, none" default:"linear"`
 	Watermark    bool   `short:"w" long:"watermark" description:"watermark print-pdf option"`
 	DisablePrint bool   `long:"disableprint" description:"remove the ability print-pdf"`
